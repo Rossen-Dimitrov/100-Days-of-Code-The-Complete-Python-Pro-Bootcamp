33fd8a4b00b4081680a56f654ad4292c76db7ace
Rosen Dimitrov <rosen.dimitrov@test.com>
2024-05-20 11:10:11 +0300

    finised password manager

--- d_29_Password_Manager_GUI_with_Tkinter/main.py
+++ d_29_Password_Manager_GUI_with_Tkinter/main.py
@@ -1,18 +1,56 @@
 from tkinter import Tk, PhotoImage, Canvas, Label, Entry, Button
+from tkinter import messagebox
+from random import randint, shuffle, choice
+from pyperclip import copy
 
 FONT_NAME = "Arial"
 
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_pass():
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(nr_letters)]
+    password_symbols = [choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_numbers + password_symbols
+
+    shuffle(password_list)
+
+    random_password = (''.join(password_list))
+    password_entry.delete(0, 'end')
+    password_entry.insert(0, random_password)
+    copy(random_password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open('passwords.txt', 'a') as file:
-        new_entry = f"{website_entry.get()}|{user_entry.get()}|{password_entry.get()}"
-        file.write(new_entry)
-        website_entry.delete(0, 'end')
-        password_entry.delete(0, 'end')
+    web_site = website_entry.get()
+    user = user_entry.get()
+    password = password_entry.get()
+
+    if len(web_site) == 0 or len(user) == 0 or len(password) == 0:
+        messagebox.showerror(title='Error', message='Empty fields are not allowed')
+    else:
+        ok = messagebox.askokcancel(title=web_site,
+                                    message=f'This are the details entered:\n'
+                                            f'Email: {user}\n'
+                                            f'Password: {password}'
+                                    )
+        if ok:
+            with open('passwords.txt', 'a') as file:
+                new_entry = f"{web_site}|{user}|{password}\n"
+                file.write(new_entry)
+                website_entry.delete(0, 'end')
+                password_entry.delete(0, 'end')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,7 +87,7 @@ password_entry.grid(row=3, column=1)
 password_entry.config()
 
 # Buttons
-password_gen_btn = Button(text='Generate Password', font=FONT_NAME)
+password_gen_btn = Button(text='Generate Password', font=FONT_NAME, command=generate_pass)
 password_gen_btn.grid(row=3, column=2)
 
 add_btn = Button(text='ADD', font=FONT_NAME, width=39, command=save)
