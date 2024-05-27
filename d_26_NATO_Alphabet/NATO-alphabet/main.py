ee5b125b4f6df2787386c17bb30517e13a749533
Rosen Dimitrov <rosen.dimitrov@test.com>
2024-05-27 16:51:19 +0300

    Nato Alphabet eexception adde

--- d_26_NATO_Alphabet/NATO-alphabet/main.py
+++ d_26_NATO_Alphabet/NATO-alphabet/main.py
@@ -3,13 +3,23 @@ import pandas
 nato_ab = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_ab_data_frame = pandas.DataFrame(nato_ab)
 
-#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 
 nato_ab_dict = {row.letter: row.code for (index, row) in nato_ab_data_frame.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-input_list = [letter.upper() for letter in input('Enter name: ')]
-nato_name = [nato_ab_dict[l] for l in input_list]
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-print(nato_name)
+def nato_alphabet():
+    input_list = input('Enter name: ').upper()
+    try:
+        nato_name = [nato_ab_dict[l] for l in input_list]
+    except KeyError:
+        print('Only letters allowed')
+        nato_alphabet()
+
+    else:
+        print(nato_name)
+
+
+nato_alphabet()
